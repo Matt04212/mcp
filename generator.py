80909cc785ce9f62dfe46471cfa528e788b340bc
黃敬惟 <matthew@huangjingweideMacBook-Air.local>
2026-03-30 00:37:19 +0800

    initial commit

---
+++ generator.py
@@ -0,0 +1,15 @@
+class Generator:
+    def __init__(self, num_edges, num_nodes):
+        self.edge = num_edges
+        self.node = num_nodes
+
+    def generate(self):
+        for i in range(self.edge):
+            edges = [i]
+            return edges
+        for i in range(self.node):
+            nodes = [i]
+            return nodes
+
+
+
