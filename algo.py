bf56c388e2e5fb9c8abeef6f37a52f09c10d5e1d
黃敬惟 <matthew@huangjingweideMacBook-Air.local>
2026-04-14 00:02:28 +0800

    update

--- algo.py
+++ algo.py
@@ -1,23 +1,35 @@
 import subprocess
 
-def algo(hg, nparts):
+def write_hgr(hg, covered_vertices, removed_edges, filname):
+    live_vtxs = [v for v in hg.vtxs if v not in covered_vertices]
+    #orginial -> hmetis id
+    v_map = {v: i+1 for i, v in enumerate(live_vtxs)}
+    #inverse
+    v_map_inv = {i+1: v for i, v in enumerate(live_vtxs)}
+
+    return v_map_inv
+
+def algo(hg, nparts, filename):
     covered_vertices = set()
     removed_edges = set()
     solu = []
 
-    subprocess.run(f"./hmetis data1.hgr {nparts} 5 10 2 1 0 0 0", shell=True)
+    v_map_inv = write_hgr(hg, covered_vertices, removed_edges, f"{filename}.part.{nparts}")
 
-    with open(f'data1.hgr.part.{nparts}', "r") as f:
+    subprocess.run(f"./hmetis {filename} {nparts} 5 10 2 1 0 0 0", shell=True)
+
+    with open(f'{filename}.part.{nparts}', "r") as f:
         line = f.read().splitlines()
 
     partitions = {}
     for vtx, part in enumerate(line, start=1):
+        original_vtx = v_map_inv[vtx]
         part = int(part)
 
         if part not in partitions:
             partitions[part] = set()
 
-        partitions[part].add(vtx)
+        partitions[part].add(original_vtx)
 
     for n in range(nparts):
         best_score = 0
@@ -29,15 +41,17 @@ def algo(hg, nparts):
                     candidate_hedge.add(hedge)
 
         for hedge in candidate_hedge:
-            score = len(hg.hedges_dict[hedge].intersection(partition[n]))
+            score = len(hg.hedges_dict[hedge].intersection(partitions[n]))
             if score > best_score:
                 best_score = score
                 best_edge = hedge
             else:
                 continue
 
-        print("edge:", best_edge)
-        solu.add(best_edge)
+        newly_coverd = hg.hedges_dict[best_edge] - covered_vertices
+        covered_vertices.update(newly_coverd)
+        removed_edges.add(best_edge)
+        solu.append(best_edge)
 
     
 
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from hypergraph import Hypergraph
 from algo import algo
 import matplotlib.pyplot as plt
 
-hg = Hypergraph(10000,10000)
+hg = Hypergraph(300,300)
 hg.generate()
 hg.output("data1.hgr")
 
-solution = algo(hg, 4)
+solution = algo(hg, 4, "data1.hgr")
