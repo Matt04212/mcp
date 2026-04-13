f3c0e59833dbe9fdfe63579b9c6af743dbafdecd
黃敬惟 <matthew@huangjingweideMacBook-Air.local>
2026-04-13 12:50:08 +0800

    update

--- algo.py
+++ algo.py
@@ -1,6 +1,10 @@
 import subprocess
 
 def algo(hg, nparts):
+    covered_vertices = set()
+    removed_edges = set()
+    solu = []
+
     subprocess.run(f"./hmetis data1.hgr {nparts} 5 10 2 1 0 0 0", shell=True)
 
     with open(f'data1.hgr.part.{nparts}', "r") as f:
@@ -13,15 +17,20 @@ def algo(hg, nparts):
         if part not in partitions:
             partitions[part] = set()
 
-        partitions[part].update(hg.vtxs_dict[vtx])
+        partitions[part].add(vtx)
 
-    solu = set()
     for n in range(nparts):
         best_score = 0
         best_edge = None
-        for hedge in hg.hedges:
-            score = len(hg.hedges_dict[hedge].intersection(partitions[n]))
-            if score > best_score and hedge not in solu:
+        candidate_hedge = set()
+        for v in partitions[n]:
+            for hedge in hg.vtxs_dict[v]:
+                if hedge not in removed_edges:
+                    candidate_hedge.add(hedge)
+
+        for hedge in candidate_hedge:
+            score = len(hg.hedges_dict[hedge].intersection(partition[n]))
+            if score > best_score:
                 best_score = score
                 best_edge = hedge
             else:
