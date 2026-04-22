d807396c6c10f657b9447dab78741e900ade76f1
黃敬惟 <matthew@huangjingweideMacBook-Air.local>
2026-04-22 14:36:53 +0800

    update

--- algo.py
+++ algo.py
@@ -14,38 +14,38 @@ def algo(hg, nparts, filename):
     iteration = 0
     last_hmetis_covered = 0
     threshhold = int(0.05 * hg.nvtxs)
-
+    partitions = {}
     while len(covered_vertices) <  hg.nvtxs:
 
         t = time.time()
         # use greedy if small enough
         live_vtxs = [v for v in hg.vtxs if v not in covered_vertices]
-        """if len(live_vtxs) < 500:
+        """if len(live_vtxs) < 400:
             greedy(hg, covered_vertices, removed_edges, solu)
 
             break"""
 
-        #if len(covered_vertices) - last_hmetis_covered >= threshhold:
+        if not partitions or len(covered_vertices) - last_hmetis_covered >= threshhold:
 
 
-        v_map_inv= write_hgr(hg, covered_vertices, removed_edges, filename, live_vtxs)
-        subprocess.run(f"./hmetis {filename} {nparts} 5 1 2 1 0 0 0", shell=True)
-        last_hmetis_covered = len(covered_vertices)
+            v_map_inv= write_hgr(hg, covered_vertices, removed_edges, filename, live_vtxs)
+            subprocess.run(f"./hmetis {filename} {nparts} 5 1 2 1 0 0 0", shell=True)
+            last_hmetis_covered = len(covered_vertices)
 
 
-        with open(f"{filename}.part.{nparts}", "r") as f:
-            line = f.read().splitlines()
+            with open(f"{filename}.part.{nparts}", "r") as f:
+                line = f.read().splitlines()
 
-        partitions = {}
-        for vtx, part in enumerate(line, start=1):
-            #print(f"partition file lines: {len(line)}, v_map_inv size: {len(v_map_inv)}")
-            original_vtx = v_map_inv[vtx]
-            part = int(part)
+            partitions = {}
+            for vtx, part in enumerate(line, start=1):
+                #print(f"partition file lines: {len(line)}, v_map_inv size: {len(v_map_inv)}")
+                original_vtx = v_map_inv[vtx]
+                part = int(part)
 
-            if part not in partitions:
-                partitions[part] = set()
+                if part not in partitions:
+                    partitions[part] = set()
 
-            partitions[part].add(original_vtx)
+                partitions[part].add(original_vtx)
 
         #pick top-k
         selection(hg, covered_vertices, removed_edges, solu, partitions)
@@ -78,10 +78,10 @@ def algo(hg, nparts, filename):
         print(f"iter{iteration}:covered = {len(covered_vertices)}, "
               f"removed_edges = {len(removed_edges)}, time = {time.time() - t:.2f}s")
 
-        if iteration == 100:
+        if iteration == 2000:
             break
 
-    return solu
+    return len(removed_edges), len(covered_vertices)
 
 
 
